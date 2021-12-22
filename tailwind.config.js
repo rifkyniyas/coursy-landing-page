05f229b17e13552d7d5f6574d5685b615e053ac0
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-12-22 21:03:38 +0530

    working on hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/hero-bg.svg')",
+      }
+    }
   },
   plugins: [],
 }
