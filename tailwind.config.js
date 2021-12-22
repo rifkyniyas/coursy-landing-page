40cd1abe22caab18b4db0e6e5c96a7d3b742586b
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-12-22 23:18:12 +0530

    completed basic version of hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+
     extend: {
       backgroundImage: {
         'hero-image': "url('/images/hero-bg.svg')",
-      }
+      },
+      colors: {
+        'light-gray': 'rgba(255, 255, 255, 0.7)',
+        'black': '#1e2432',
+        'hover-gray': 'rgba(255, 255, 255, 0.8)',
+        'hover-black': '#4b505b'
+      },
     }
   },
   plugins: [],
