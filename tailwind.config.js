a229eccb50fef7515dd6b3de4a3e23d87e8d7e67
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-12-23 23:08:29 +0530

    completed footer and cta section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,14 @@ module.exports = {
         'cta-image': "url('/images/cta-bg.svg')"
       },
       colors: {
+        primary : '#01216e',
         gray : {
           light: 'rgba(255, 255, 255, 0.7)',
           hover: 'rgba(255, 255, 255, 0.8)',
           dark: '#eef2f5',
           text: "#6f737c"
         },
-        'black': '#1e2432',
+        black: '#1e2432',
         'hover-black': '#4b505b'
       },
     }
