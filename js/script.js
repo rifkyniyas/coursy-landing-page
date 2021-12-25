1e50affa2c32417291d725ae9d008b1deb62bb0e
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-12-25 21:19:31 +0530

    improved responsiveness

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ const navbar = document.getElementById('navbar')
 const toggleNavbar = () => {
     navbar.classList.toggle('hidden')
 }
+window.onclick = (event) => {
+    if (!event.target.matches('#toggleBtn') && !navbar.classList.contains('hidden')) {
+        toggleNavbar()
+    }
+}
